40030f3c8b265c84a2191c0dc3c4368445b3c46a
aakash-10 <54438165+aakash-10@users.noreply.github.com>
2020-04-25 01:13:34 +0530

    url fixing done and started featured products

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {linkData} from './linkData'
 import { socialData} from './socialData'
+import {productData, items} from './productData'
 const ProductContext = React.createContext();
 //provider
 //consumer
@@ -9,11 +10,83 @@ class ProductProvider extends Component{
     state={
         sidebarOpen : false,
         cartOpen :false,
-        carItems: 10,
+        carItems: 0,
         links: linkData,
         socialIcons: socialData,
-        cart : []
+        cart : [],
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0,
+        storeProducts:[],
+        featuredProducts:[],
+        singleProduct:{},
+        loading:true
+
     }
+componentDidMount(){
+    //from contentful
+
+
+
+    this.setProducts(items)
+}
+
+//set Products
+setProducts = (products)=>{
+    let storeProducts = products.map(item=>{
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        const product = {id,...item.fields, image};
+        return product
+    });
+
+//featured products
+
+let featuredProducts = storeProducts.filter(item => item.featured== true);
+this.setState({
+    storeProducts,
+    filteredProducts: storeProducts,
+    featuredProducts,
+    cart:this.getStorageCart(),
+    singleProduct: this.getStorageProduct(),
+    loading: false
+})
+}
+
+//get cart from local storage
+getStorageCart = () =>{
+    return []
+}
+
+//get product from local storage
+getStorageProduct = () => {
+        return []
+    }
+
+    //get totals
+getTotals = () =>{
+
+}
+
+//add totals
+addTotals =() =>{
+
+}
+
+//sync storage
+syncStorage = () =>{}
+
+//add to cart
+addToCart = (id) =>{
+    console.log(`add to cart ${id}`)
+}
+
+//set single product
+
+setSingleProduct = (id) =>{
+    console.log(`set single product ${id}`)
+}
+
 // handle sidebar
     handleSidebar = () =>{
        
@@ -39,7 +112,7 @@ class ProductProvider extends Component{
 
     render(){
         return (<ProductContext.Provider value={{handleSidebar:this.handleSidebar,
-        ...this.state,handleCart:this.handleCart,closeCart:this.closeCart,openCart:this.openCart}}>
+        ...this.state,handleCart:this.handleCart,closeCart:this.closeCart,openCart:this.openCart,addToCart:this.addToCart,setSingleProduct:this.setSingleProduct}}>
             {this.props.children}
         </ProductContext.Provider>)
        
